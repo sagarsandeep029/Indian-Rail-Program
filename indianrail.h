db9c5266f936cd57dd6a3c42eb8069d4a7d15818
sagarsandeep029 <44023569+sagarsandeep029@users.noreply.github.com>
2019-04-07 20:25:19 +0530

    Add files via upload

---
+++ indianrail.h
@@ -0,0 +1,17 @@
+#include <pthread.h>
+struct station {
+  int out_passengers;
+  int in_passengers;
+  pthread_mutex_t lock;
+  pthread_cond_t train_arrived_cond;
+  pthread_cond_t passengers_seated_cond;
+  pthread_cond_t train_is_full_cond;
+};
+
+void station_init(struct station *station);
+
+void station_load_train(struct station *station, int count);
+
+void station_wait_for_train(struct station *station);
+
+void station_on_board(struct station *station);
